50228d389d7c88bb76698eceec20fcbcff62b942
matheus.leal <matheus.leal@g.globo>
2021-08-29 20:34:59 -0300

    bug fix on iota bulk data store

--- ozy/iota/data.go
+++ ozy/iota/data.go
@@ -1,96 +1,154 @@
 package iota
 
 import (
-  . "github.com/iotaledger/iota.go/api"
-  "github.com/iotaledger/iota.go/trinary"
-  "github.com/iotaledger/iota.go/converter"
-  "github.com/iotaledger/iota.go/bundle"
-  "strings"
-  // "log"
-  "bytes"
-  "sort"
+	"bytes"
+	"log"
+	"sort"
+	"strings"
+
+	. "github.com/iotaledger/iota.go/api"
+	"github.com/iotaledger/iota.go/bundle"
+	"github.com/iotaledger/iota.go/converter"
+	"github.com/iotaledger/iota.go/trinary"
 )
 
 const mwm = 9
 const depth = 3
 
 func StoreData(endpoint string, seed string, address string, data string, tag string) {
-  // Compose a new API instance
-  api, err := ComposeAPI(HTTPClientSettings{URI: endpoint})
-  if err != nil {
-    panic(err)
-  }
-  // Convert a ascii message for the transaction to trytes,if possible
-  message, err := converter.ASCIIToTrytes(data)
-  if err != nil {
-    panic(err)
-  }
-  transfers := bundle.Transfers{
-    {
-      Address: address,
-      Value: 0,
-      Message: message,
-      Tag: trinary.Trytes(tag),
-    },
-  }
-  // We need to pass an options object, since we want to use the defaults it stays empty
-  prepTransferOpts := PrepareTransfersOptions{}
-  trytes, err := api.PrepareTransfers(seed, transfers, prepTransferOpts)
-  if err != nil {
-    panic(err)
-  }
-  // Send the transaction to the tangle using given depth and minimum weight magnitude
-  _, err = api.SendTrytes(trytes, depth, mwm)
-  // bndl, err := api.SendTrytes(trytes, depth, mwm)
-  if err != nil {
-    panic(err)
-  }
-  // var txhash = bundle.TailTransactionHash(bndl)
-  // log.Println("[IOTA] DEBUG: broadcasted bundle with tail tx hash: ", txhash)
-  // log.Printf("[IOTA] DEBUG: https://explorer.iota.org/legacy-devnet/transaction/%s\n\n", txhash)
+	// Compose a new API instance
+	api, err := ComposeAPI(HTTPClientSettings{URI: endpoint})
+	if err != nil {
+		panic(err)
+	}
+	// Convert a ascii message for the transaction to trytes,if possible
+	message, err := converter.ASCIIToTrytes(data)
+	if err != nil {
+		panic(err)
+	}
+	transfers := bundle.Transfers{
+		{
+			Address: address,
+			Value:   0,
+			Message: message,
+			Tag:     trinary.Trytes(tag),
+		},
+	}
+	// We need to pass an options object, since we want to use the defaults it stays empty
+	prepTransferOpts := PrepareTransfersOptions{}
+	trytes, err := api.PrepareTransfers(seed, transfers, prepTransferOpts)
+	if err != nil {
+		panic(err)
+	}
+	// Send the transaction to the tangle using given depth and minimum weight magnitude
+	_, err = api.SendTrytes(trytes, depth, mwm)
+	// bndl, err := api.SendTrytes(trytes, depth, mwm)
+	if err != nil {
+		panic(err)
+	}
+	var txhash = bundle.TailTransactionHash(bndl)
+	log.Println("[IOTA] DEBUG: broadcasted bundle with tail tx hash: ", txhash)
+	log.Printf("[IOTA] DEBUG: https://explorer.iota.org/legacy-devnet/transaction/%s\n\n", txhash)
 }
 
-func RetriveData(endpoint string, address string, tag string){
-  // Compose a new API instance
-  api, err := ComposeAPI(HTTPClientSettings{URI: endpoint})
-  if err != nil {
-    panic(err) 
-  }
-  // We need a query object containing the address we want to look for
-  var modifiedTag = tag + strings.Repeat("9", 27 - len(tag))
-  var query = FindTransactionsQuery{Tags: trinary.Hashes{trinary.Trytes(modifiedTag)}}
-  // Find Transaction Objects uses the connected node to find transactions based on our query
-  transactions, err := api.FindTransactionObjects(query)
-  if err != nil {
-    panic(err) 
-  }
-  // We need to sort all transactions by index first so we can concatenate them
-  sort.Slice(transactions[:], func(i, j int) bool {
-    return transactions[i].CurrentIndex < transactions[j].CurrentIndex
-  })
-  // We define a buffer to concatenate all sorted transactions
-  var buffer bytes.Buffer
-  // We add the sorted Transaction Signature Message Fragment to the buffer
-  for _, tx := range transactions {
-    buffer.WriteString(tx.SignatureMessageFragment)
-  }
-  // // We need to convert the message to ASCII, but before we do that we need to remove 
-  // msg, err := converter.TrytesToASCII(removeSuffixNine(buffer.String()))
-  // if err != nil {
-  //   panic(err) 
-  // }
-  // // We print out the message
-  // log.Println("[IOTA] DEBUG: Query Result ", msg)
+func BulkData(endpoint string, seed string, address string, bulk []string, tag string) {
+	// Compose a new API instance
+	api, err := ComposeAPI(HTTPClientSettings{URI: endpoint})
+	if err != nil {
+		panic(err)
+	}
+	var transfers bundle.Transfers
+	limit := 200
+	for i := 0; i < len(bulk); i += limit {
+		batch := bulk[i:min(i+limit, len(bulk))]
+		transfers = prepareBulkArray(address, batch, tag)
+		// We need to pass an options object, since we want to use the defaults it stays empty
+		prepTransferOpts := PrepareTransfersOptions{}
+		trytes, err := api.PrepareTransfers(seed, transfers, prepTransferOpts)
+		if err != nil {
+			panic(err)
+		}
+		// Send the transaction to the tangle using given depth and minimum weight magnitude
+		bndl, err := api.SendTrytes(trytes, depth, mwm)
+		// _, err = api.SendTrytes(trytes, depth, mwm)
+		if err != nil {
+			panic(err)
+		}
+		var txhash = bundle.TailTransactionHash(bndl)
+		log.Println("[IOTA] DEBUG: broadcasted bundle with tail tx hash: ", txhash)
+		// log.Printf("[IOTA] DEBUG: https://explorer.iota.org/legacy-devnet/transaction/%s\n\n", txhash)
+	}
+}
+
+func RetriveData(endpoint string, address string, tag string) {
+	// Compose a new API instance
+	api, err := ComposeAPI(HTTPClientSettings{URI: endpoint})
+	if err != nil {
+		panic(err)
+	}
+	// We need a query object containing the address we want to look for
+	var modifiedTag = tag + strings.Repeat("9", 27-len(tag))
+	var query = FindTransactionsQuery{Tags: trinary.Hashes{trinary.Trytes(modifiedTag)}}
+	// Find Transaction Objects uses the connected node to find transactions based on our query
+	transactions, err := api.FindTransactionObjects(query)
+	if err != nil {
+		panic(err)
+	}
+	// We need to sort all transactions by index first so we can concatenate them
+	sort.Slice(transactions[:], func(i, j int) bool {
+		return transactions[i].CurrentIndex < transactions[j].CurrentIndex
+	})
+	// We define a buffer to concatenate all sorted transactions
+	var buffer bytes.Buffer
+	// We add the sorted Transaction Signature Message Fragment to the buffer
+	for _, tx := range transactions {
+		buffer.WriteString(tx.SignatureMessageFragment)
+	}
+	// We need to convert the message to ASCII, but before we do that we need to remove
+	msg, err := converter.TrytesToASCII(removeSuffixNine(buffer.String()))
+	if err != nil {
+		panic(err)
+	}
+	// We print out the message
+	log.Println("[IOTA] DEBUG: Query Result ", msg)
+}
+
+func prepareBulkArray(address string, bulk []string, tag string) bundle.Transfers {
+	var transfers bundle.Transfers
+	for _, data := range bulk {
+		// Convert a ascii message for the transaction to trytes,if possible
+		message, err := converter.ASCIIToTrytes(data)
+		if err != nil {
+			panic(err)
+		}
+		// Create a transfer
+		var transfer bundle.Transfer = bundle.Transfer{
+			Address: address,
+			Value:   0,
+			Message: message,
+			Tag:     trinary.Trytes(tag),
+		}
+		// Bulk multiple transfers to reduce time
+		transfers = append(transfers, transfer)
+	}
+	return transfers
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
 }
 
 func removeSuffixNine(frag string) string {
-  fraglen := len(frag)
-  var firstNonNineAt int
-  for i := fraglen - 1; i > 0; i-- {
-    if frag[i] != '9' {
-      firstNonNineAt = i
-      break;
-    }
-  }
-  return frag[:firstNonNineAt+1]
+	fraglen := len(frag)
+	var firstNonNineAt int
+	for i := fraglen - 1; i > 0; i-- {
+		if frag[i] != '9' {
+			firstNonNineAt = i
+			break
+		}
+	}
+	return frag[:firstNonNineAt+1]
 }
--- ozy/main.go
+++ ozy/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	iotaHandler "github.com/matheusroleal/ozymandias/ozy/iota"
-  asset "github.com/matheusroleal/ozymandias/ozy/asset"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/iotaledger/iota.go/trinary"
-  "encoding/json"
-  "time"
-  "fmt"
-  "math/rand"
+	"github.com/matheusroleal/ozymandias/ozy/asset"
+	iotaHandler "github.com/matheusroleal/ozymandias/ozy/iota"
 )
 
-const endpoint = "https://nodes.devnet.iota.org"
+// const endpoint = "https://nodes.devnet.iota.org"
+const endpoint = "https://nodes.devnet.iota.org:443"
+
 // We need a dummy seed even though we don't sign, because the API requires a seed to send
 const seed = trinary.Trytes("JBN9ZRCOH9YRUGSWIQNZWAIFEZUBDUGTFPVRKXWPAUCEQQFS9NHPQLXCKZKRHVCCUZNF9CZZWKXRZVCWQ")
 const address = trinary.Trytes("XBN9ZRCOH9YRUGSWIQNZWAIFEZUBDUGTFPVRKXWPAUCEQQFS9NHPQLXCKZKRHVCCUZNF9CZZWKXRZVCWQMZOCAHYPD")
@@ -18,33 +21,40 @@ const address = trinary.Trytes("XBN9ZRCOH9YRUGSWIQNZWAIFEZUBDUGTFPVRKXWPAUCEQQFS
 var letterRunes = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func RandStringRunes(n int) string {
-    b := make([]rune, n)
-    for i := range b {
-        b[i] = letterRunes[rand.Intn(len(letterRunes))]
-    }
-    return string(b)
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
 
 func main() {
-  // Create blocks of data with tags
-  for j := 0; j < 43800; j++ {
-    var tag = RandStringRunes(25)
-    // Our data is very long here, it needs to be split over several transactions, 3 in this case
-    sum := 0
-    for i := 0; i < 2628002; i++ {
-      checkpoint := asset.CreateCheckpoint(fmt.Sprintf("%s%d","checkpoint ", sum), fmt.Sprintf("%d",j))
-      b, err := json.Marshal(checkpoint)
-      if err != nil {
-          panic(err)
-      }
-      data := string(b)
-      iotaHandler.StoreData(endpoint, seed, address, data, tag)
-      sum += i
-    }
-    t1 := time.Now()
-    iotaHandler.RetriveData(endpoint, address, tag)
-    t2 := time.Now()
-    diff := t2.Sub(t1)
-    fmt.Println(diff)
-  }
+	// Create blocks of data with tags
+	for j := 0; j < 31; j++ {
+		var tag = RandStringRunes(25)
+		// var tag = "XVLBZGBAICMRAJWWHTHCTCUAX"
+		fmt.Println(tag)
+		// Our data is very long here, it needs to be split over several transactions, 3 in this case
+		sum := 0
+		var bulk []string
+		for i := 0; i < 100000; i++ { // Simulating 84 hours per week data
+			checkpoint := asset.CreateCheckpoint(fmt.Sprintf("%s%d", "checkpoint ", sum), fmt.Sprintf("%d", j))
+			b, err := json.Marshal(checkpoint)
+			if err != nil {
+				panic(err)
+			}
+			data := string(b)
+			// iotaHandler.StoreData(endpoint, seed, address, data, tag)
+			bulk = append(bulk, data)
+			// fmt.Println(i)
+			sum += i
+		}
+		fmt.Println("Sending Messages in a Bulk")
+		iotaHandler.BulkData(endpoint, seed, address, bulk, tag)
+		t1 := time.Now()
+		iotaHandler.RetriveData(endpoint, address, tag)
+		t2 := time.Now()
+		diff := t2.Sub(t1)
+		fmt.Println(diff)
+	}
 }
